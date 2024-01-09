4c7c6ee9ea3588fa81c6e641dbd1e50a04247869
josvan87 <josvansouza@gmail.com>
2024-01-09 14:43:44 -0300

    O PAI TÁ ON

---
+++ heroi.js
@@ -0,0 +1,13 @@
+let nomeHeroi = "Josvan"
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+    if(){
+
+    }else if(){
+
+    }else if(){
+        
+    }
+}
