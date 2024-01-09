f9ab4639524e0ddc922e3211ceb1c8a4013b7e6c
josvan87 <josvansouza@gmail.com>
2024-01-09 17:05:03 -0300

    suave na nave

--- heroi.js
+++ heroi.js
@@ -1,5 +1,29 @@
 
-let nomeHeroi = "Josvan"
-let quantidadeDeEsperiencia = [];
+let nomeHeroi = "Josvan";
+let xp = 1500;
 
+if( xp < 1000){
+    console.log("O herói de nome josvan está no nível de FERRO");
 
+}else if( xp > 1001 && xp < 2000){
+    console.log("O herói de nome josvan está no nível de BRONZE");
+ 
+}else if( xp >  2001 && xp < 5000){
+    console.log("O herói de nome josvan está no nível de PRATA");
+
+}else if( xp >  5001 && xp < 7000){
+    console.log("O herói de nome josvan está no nível de OURO");
+
+}else if( xp >  7001 && xp < 8000){
+    console.log("O herói de nome josvan está no nível de PLATINA");
+
+}else if( xp >  8001 && xp < 9000){
+    console.log("O herói de nome josvan está no nível de ASCENDENTE");
+
+}else if( xp >  9001 && xp < 10000){
+    console.log("O herói de nome josvan está no nível de IMORTAL");
+}else{
+    console.log("RADIANTE");
+}
+
+console.log();
